86eac49be681a6ba3785552963e93fcd2be60029
yatee-singh <f20212534@goa.bits-pilani.ac.in>
2024-09-27 23:50:59 +0530

    aded mapping locations

--- frontend/src/pages/MapLocations.jsx
+++ frontend/src/pages/MapLocations.jsx
@@ -2,38 +2,148 @@
 import BookTickets from "./BookTickets";
 import Desp from "./Desp";
 import Header from "./Header";
-import { Button } from "@chakra-ui/react";
+
 //import Button from 'react-bootstrap/Button';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+  MarkerF
+} from '@react-google-maps/api'
+import { useRef, useState } from 'react'
+const center = { lat: 48.8584, lng: 2.2945 }
+const markers = [
+  {
+    id: 1,
+    name: "Panaji",
+    position: { lat: 15.4909, lng: 73.8278 },
+  }, 
+  {
+    id: 2,
+    name: "Margao",
+    position: { lat: 15.2993, lng: 73.9570 },
+  },
+  {
+    id: 3,
+    name: "Vasco da Gama",
+    position: { lat: 15.3860, lng: 73.8443 },
+  },
+  {
+    id: 4,
+    name: "Mapusa",
+    position: { lat: 15.5912, lng: 73.8087 },
+  },
+  {
+    id: 5,
+    name: "Ponda",
+    position: { lat: 15.4047, lng: 74.0150 },
+  }
+];
 function MapLocations() {
+    const navigate = useNavigate();
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyAC_hK0PSk7fUcavQM8vck5Dy_zmXZvTsQ',
+    libraries: ['places'],
+  })
+  
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+
+  
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef()
+
+  if (!isLoaded) {
+    return <div>Laoding...</div>
+  }
+
+  function findCenterLatLng(markers) {
+    let totalLat = 0;
+    let totalLng = 0;
+    let totalMarkers = markers.length;
+
+    markers.forEach(marker => {
+        totalLat += marker.position.lat;
+        totalLng += marker.position.lng;
+    });
+
+    let centerLat = totalLat / totalMarkers;
+    let centerLng = totalLng / totalMarkers;
+
+    return { lat: centerLat, lng: centerLng };
+}
+
 
  
-  const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate('/booking');
 
-  };
+  
   return (
     <div >
     
-<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"><a onClick={()=>{navigate('/search')}}> {"<"}    </a>Map</h1>
-<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white" ><a onClick={()=>{navigate('/search')}}> {"<"}    </a >Map</h1>
+<Box height={'90vh' } width={'100vw'}>
+  <Box  left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={findCenterLatLng(markers)}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={map => setMap(map)}
+        >
+          {/* <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )} */}
+
+           {markers.map(({ id, name, position }) => (
+                <MarkerF
+                  key={id}
+                  position={position}
+                  onClick={() => handleActiveMarker(id,position)}
+                  // icon={{
+                  //   url:"https://t4.ftcdn.net/jpg/02/85/33/21/360_F_285332150_qyJdRevcRDaqVluZrUp8ee4H2KezU9CA.jpg",
+                  //   scaledSize: { width: 50, height: 50 }
+                  // }}
+                >
+                 {/* {activeMarker === id ? (
+                    <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+                      <div>
+                        <p>{name}</p>
+                      </div>
+                    </InfoWindowF>
+                  ) : null} */}
+                </MarkerF>
+              ))}
+        </GoogleMap>
+        </Box>
+
+</Box>
 
    
 
-<div class="inline-flex rounded-md shadow-sm" role="group">
-  <button onClick={()=>{navigate('analytics');
-}} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-    Analytics
-  </button>
-  <button onClick={()=>{navigate('/user');
-}} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-    Search Parking Spaces
-  </button>
- 
-</div>
 
    
     </div>
